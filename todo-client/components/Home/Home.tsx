e440d4b708a907463413500183da1110eaa81c79
shitakemura <shitakemura@gmail.com>
2022-02-08 16:48:51 +0900

    Introduce apollo client

--- todo-client/components/Home/Home.tsx
+++ todo-client/components/Home/Home.tsx
@@ -1,12 +1,20 @@
 import { VStack } from "@chakra-ui/react";
 import { Header } from "../Header";
 import { TodoScreen } from "../Todo";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_APPSYNC_API_URL,
+  cache: new InMemoryCache(),
+});
 
 export const Home = () => {
   return (
     <VStack>
-      <Header />
-      <TodoScreen />
+      <ApolloProvider client={client}>
+        <Header />
+        <TodoScreen />
+      </ApolloProvider>
     </VStack>
   );
 };
