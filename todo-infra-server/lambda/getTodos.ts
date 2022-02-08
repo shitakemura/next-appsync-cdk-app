9db25eac1af08ad377f5a16f40af3dab13ab7bec
shitakemura <shitakemura@gmail.com>
2022-02-08 14:48:05 +0900

    Create getTodos query

---
+++ todo-infra-server/lambda/getTodos.ts
@@ -0,0 +1,25 @@
+import { AppSyncResolverHandler } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import { Todo } from "../graphql/generated/generated-types";
+
+const docClient = new DynamoDB.DocumentClient();
+
+export const handler: AppSyncResolverHandler<
+  null,
+  Todo[] | null
+> = async () => {
+  try {
+    if (!process.env.TODO_TABLE) {
+      console.log("TODO_TABLE was not specified");
+      return null;
+    }
+
+    const data = await docClient
+      .scan({ TableName: process.env.TODO_TABLE })
+      .promise();
+    return data.Items as Todo[];
+  } catch (err) {
+    console.error(`[Error] DynamoDB error: ${err}`);
+    return null;
+  }
+};
--- todo-infra-server/lib/todo-infra-server-stack.ts
+++ todo-infra-server/lib/todo-infra-server-stack.ts
@@ -1,16 +1,69 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as appsync from "@aws-cdk/aws-appsync-alpha";
+import { Construct } from "constructs";
 
-export class TodoInfraServerStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class TodoInfraServerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB
+    const todoTable = new dynamodb.Table(this, "TodoTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoInfraServerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // AppSync
+    const todoApi = new appsync.GraphqlApi(this, "TodoGraphqlApi", {
+      name: "todo-graphql-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
+        },
+      },
+    });
+
+    // Lambda function
+    const getTodosLambda = new lambdaNodeJs.NodejsFunction(
+      this,
+      "getBooksHandler",
+      {
+        entry: "lambda/getTodos.ts",
+        handler: "handler",
+        environment: {
+          TODO_TABLE: todoTable.tableName,
+        },
+      }
+    );
+
+    todoTable.grantReadData(getTodosLambda);
+
+    // AppSync DataSource
+    const getTodosDataSource = todoApi.addLambdaDataSource(
+      "getTodosDataSource",
+      getTodosLambda
+    );
+
+    getTodosDataSource.createResolver({
+      typeName: "Query",
+      fieldName: "getTodos",
+    });
+
+    // CfnOutput
+    new cdk.CfnOutput(this, "GraphQlApiUrl", {
+      value: todoApi.graphqlUrl,
+    });
+
+    new cdk.CfnOutput(this, "GraphQlApiKey", {
+      value: todoApi.apiKey || "",
+    });
   }
 }
