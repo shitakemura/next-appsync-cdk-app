8cf60ac0a93f0555080cb21593dd9dbaa4a6e4c5
shitakemura <shitakemura@gmail.com>
2022-02-09 21:26:42 +0900

    Set token.sub to userId condition of getTodos

--- todo-infra-server/lambda/getTodos.ts
+++ todo-infra-server/lambda/getTodos.ts
@@ -1,13 +1,16 @@
 import { AppSyncResolverHandler } from "aws-lambda";
 import { DynamoDB } from "aws-sdk";
 import { Todo } from "../graphql/generated/generated-types";
+import jwt_decode, { JwtPayload } from "jwt-decode";
 
 const docClient = new DynamoDB.DocumentClient();
 
-export const handler: AppSyncResolverHandler<
-  null,
-  Todo[] | null
-> = async () => {
+export const handler: AppSyncResolverHandler<null, Todo[] | null> = async (
+  event
+) => {
+  const token = event.request.headers.authorization ?? "";
+  const sub = jwt_decode<JwtPayload>(token).sub ?? "";
+
   try {
     if (!process.env.TODO_TABLE) {
       console.log("TODO_TABLE was not specified");
@@ -19,7 +22,7 @@ export const handler: AppSyncResolverHandler<
         TableName: process.env.TODO_TABLE,
         KeyConditionExpression: "userId = :u",
         ExpressionAttributeValues: {
-          ":u": "user001",
+          ":u": sub,
         },
       })
       .promise();
